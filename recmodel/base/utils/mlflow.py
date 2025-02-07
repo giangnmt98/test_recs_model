f4c5ffeb9fc2eb3e85119b362f6c72ce7ec7f78f
Nguyễn Mậu Trường Giang <giangnmt98@gmail.com>
2025-02-07 15:19:05 +0700

    Rename default MySQL database constant for clarity
    Renamed `DEFAULT_MYSQL_DATABASE` to `DEFAULT_MYSQL_MLFLOW_DATABASE` to more explicitly indicate its purpose. This improves readability and reduces potential confusion when configurations.

--- recmodel/base/utils/mlflow.py
+++ recmodel/base/utils/mlflow.py
@@ -33,7 +33,7 @@ class MLflowMaster(metaclass=SingletonMeta):
         self.mysql_password = os.getenv("MYSQL_PASSWORD", DEFAULT_MYSQL_PASSWORD)
         self.mysql_host = os.getenv("MYSQL_HOST", DEFAULT_MYSQL_HOST)
         self.mysql_port = os.getenv("MYSQL_PORT", DEFAULT_MYSQL_PORT)
-        self.mysql_database = os.getenv("DEFAULT_MYSQL_MLFLOW_DATABASE", DEFAULT_MYSQL_MLFLOW_DATABASE)
+        self.mysql_database = os.getenv("MYSQL_MLFLOW_DATABASE", DEFAULT_MYSQL_MLFLOW_DATABASE)
         self.mlflow = mlflow
         self.use_tracking_server = (
             use_tracking_server  # New attribute to toggle between SQLite and MySQL
